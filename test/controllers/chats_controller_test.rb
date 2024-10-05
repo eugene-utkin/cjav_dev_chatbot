d9ce7026123262b074828630e4fd2576161b08dc
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-10-05 21:58:43 +0500

    fixed typo in controller test

--- test/controllers/chats_controller_test.rb
+++ test/controllers/chats_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class ChatsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index," do
-    get chats_index,_url
+  test "should get index" do
+    get chats_index_url
     assert_response :success
   end
 
