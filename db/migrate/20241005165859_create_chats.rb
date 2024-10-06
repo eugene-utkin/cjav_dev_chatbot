f0b45ebe60f40275e6111fa647ae3c5291e1e89e
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-10-06 18:13:50 +0500

    null false added to session_id

--- db/migrate/20241005165859_create_chats.rb
+++ db/migrate/20241005165859_create_chats.rb
@@ -1,7 +1,7 @@
 class CreateChats < ActiveRecord::Migration[7.1]
   def change
     create_table :chats do |t|
-      t.string :session_id
+      t.string :session_id, null: false
 
       t.timestamps
     end
