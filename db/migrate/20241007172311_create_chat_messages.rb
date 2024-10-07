f3ca6827a332bac75d33c41a2050c3258954dfa2
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-10-07 22:27:45 +0500

    default role added

--- db/migrate/20241007172311_create_chat_messages.rb
+++ db/migrate/20241007172311_create_chat_messages.rb
@@ -3,7 +3,7 @@ class CreateChatMessages < ActiveRecord::Migration[7.1]
     create_table :chat_messages do |t|
       t.references :chat, null: false, foreign_key: true
       t.text :content
-      t.string :role
+      t.string :role, default: "user"
 
       t.timestamps
     end
