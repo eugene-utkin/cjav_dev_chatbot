57ca46c80902d77a16c17c0a579fdb46ceb64537
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-10-07 22:28:36 +0500

    content limit added

--- db/migrate/20241007172311_create_chat_messages.rb
+++ db/migrate/20241007172311_create_chat_messages.rb
@@ -2,7 +2,7 @@ class CreateChatMessages < ActiveRecord::Migration[7.1]
   def change
     create_table :chat_messages do |t|
       t.references :chat, null: false, foreign_key: true
-      t.text :content
+      t.text :content, limit: 4096
       t.string :role, default: "user"
 
       t.timestamps
