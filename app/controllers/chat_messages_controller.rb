6544eefb5bd82fec6f6ae0a109329b7d22e0e767
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-10-12 19:11:56 +0500

    save added

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -2,6 +2,7 @@ class ChatMessagesController < ApplicationController
   def create
     @chat = Chat.find(params[:chat_id])
     @chat_message = @chat.chat_messages.create(chat_message_params)
+    @chat_message.save
   end
 
   private
