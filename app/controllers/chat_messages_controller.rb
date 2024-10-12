3bd5407bc2524e4b47bce45a7c7e3a9f0286d7d4
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-10-12 19:18:37 +0500

    chat id used

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -3,7 +3,10 @@ class ChatMessagesController < ApplicationController
     @chat = Chat.find(params[:chat_id])
     @chat_message = @chat.chat_messages.create(chat_message_params)
     if @chat_message.save
-      render turbo_stream: turbo_stream.append(:chat_messages, @chat_message)
+      render turbo_stream: turbo_stream.append(
+        "chat_messages_#{@chat.id}",
+        @chat_message
+      )
     end
   end
 
