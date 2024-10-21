42102b42f60c6fd0c386daf776f27ab38e60ef92
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-10-21 20:56:43 +0500

    response variable created

--- app/jobs/chat_message_created_job.rb
+++ app/jobs/chat_message_created_job.rb
@@ -5,6 +5,7 @@ class ChatMessageCreatedJob < ApplicationJob
     @chat_message = chat_message
 
     # Send those to open ai
+    response = client.chat()
 
     # Send the response back to the chat
   end
