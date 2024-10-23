2a7fb0285f0697c580f084b383e0775e4d7df887
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-10-23 21:13:24 +0500

    locals used

--- app/jobs/chat_message_created_job.rb
+++ app/jobs/chat_message_created_job.rb
@@ -26,7 +26,8 @@ class ChatMessageCreatedJob < ApplicationJob
     Turbo::StreamsChannel.broadcast_append_to(
       "chat_messages_#{chat_message.chat.id}",
       target: "chat_messages_#{chat_message.chat.id}",
-      partial: "chat_messages/chat_message"
+      partial: "chat_messages/chat_message",
+      locals:{}
     )
   end
 
